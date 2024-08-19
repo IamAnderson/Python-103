294372fd0a47e4d3bf29a3cf908de58daa087cd5
Omorogbe Anderson <omorogbeandersonsimeon@gmail.com>
2024-08-19 21:32:41 +0200

    1: Variables

---
+++ main.py
@@ -0,0 +1,9 @@
+# first_name = "Anderson"
+# last_name = "Simeon"
+# full_name = first_name + " " + last_name
+
+# print("Hello " + full_name)
+
+height = 186.5
+
+print("Your height is: ", str(height) + "cm")
