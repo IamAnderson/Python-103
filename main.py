3ccccaf9625a27fe74319dfa8cfa165434c8e294
Omorogbe Anderson <omorogbeandersonsimeon@gmail.com>
2024-08-22 10:46:52 +0200

    8: And, Or, Not

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-age = int(input("How old are you tho?? :"))
+temp = int(input("What is the temperature outside?: "));
 
-if age >= 18:
-    print("Ohh, you are off age, nice")
-    ans = input("Do you like white chocolate ?? :")
-    if ans == "Yes":
-        print("Uhmm, subarashi,, hehe")
-    elif ans == "No":
-        print("Hmm, okay, what do you like then")
-    else:
-        print("Answer Yes or No")
-else:
-    print("You don't belong here kid")
+if not (temp >= 0 and temp <= 30):
+    print("Normie Temp")
+elif not (temp > 0 or temp < -5):
+    print("Cold Normie Temp")
+    
