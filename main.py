fca37203d180f09e9d5cc36a306eaab4619e304e
Omorogbe Anderson <omorogbeandersonsimeon@gmail.com>
2024-08-22 19:12:20 +0200

    12: Break, continue and pass

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-rows = int(input("How many rows?: "))
-columns = int(input("How many columns?: "))
-symbol = input("Enter a symbol to use: ")
+# break = used to terminate loop
+# continue = skips to the next iteration of the loop
+# pass = does nothing, acts as a placeholder
 
-for i in range(rows):
-    for j in range(columns):
-         print(symbol, end="")
-    print()
+# while True:
+#     name = input("What is your name: ")
+#     if name != "":
+#         break
+
+
+# phone_number = "123-456-789"
+
+# for i in phone_number:
+#     if i == "-":
+#         continue
+#     print(i, end="")
+
+
+for i in range(1, 21):
+    if i == 13:
+        pass
+    else:
+        print(i)
