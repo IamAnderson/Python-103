00a571bf4d3d1bbf1839e3f676e37231cf4439a8
Omorogbe Anderson <omorogbeandersonsimeon@gmail.com>
2024-08-22 18:41:21 +0200

    11: Nested Loops

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-import time
+rows = int(input("How many rows?: "))
+columns = int(input("How many columns?: "))
+symbol = input("Enter a symbol to use: ")
 
-# for i in range(10):
-#     print(i+1)
-
-# for i in range(50, 100 + 1, 2):
-# #     print(i)
-
-# for i in "Anderson":
-#     print(i)
-
-for seconds in range(10, 0, -2):
-    print(seconds)
-    time.sleep(1)
-print("Happy new year")
+for i in range(rows):
+    for j in range(columns):
+         print(symbol, end="")
+    print()
